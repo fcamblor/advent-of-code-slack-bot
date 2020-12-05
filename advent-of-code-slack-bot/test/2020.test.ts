22919e7107373073e9af5a2d2d28f06909677c12
fcamblor <fcamblor@gmail.com>
2020-12-05 13:40:06 +0100

    moved 2020 tests into a dedicated directory

--- advent-of-code-slack-bot/test/2020.test.ts
+++ advent-of-code-slack-bot/test/2020.test.ts
@@ -2,5 +2,5 @@ import {testArticle} from "./tests-utils";
 
 
 for(var i=1; i<=5; i++) {
-    testArticle(`2020-${i<10?'0':''}${i}`);
+    testArticle("2020", `2020-${i<10?'0':''}${i}`);
 }
--- advent-of-code-slack-bot/test/tests-utils.ts
+++ advent-of-code-slack-bot/test/tests-utils.ts
@@ -2,10 +2,10 @@ import {htmlToSlackMarkdown} from "../main/utils";
 import * as fs from "fs";
 
 
-export function testArticle(date: string) {
+export function testArticle(directory: string, date: string) {
     test('Article conversion for '+date, () => {
-        const article = fs.readFileSync(`./test/${date}.article.html`).toString('utf-8');
-        const expectation = fs.readFileSync(`./test/${date}.expectation.md`).toString('utf-8');
+        const article = fs.readFileSync(`./test/${directory}/${date}.article.html`).toString('utf-8');
+        const expectation = fs.readFileSync(`./test/${directory}/${date}.expectation.md`).toString('utf-8');
         expect(htmlToSlackMarkdown(article).trim()).toBe(expectation.trim());
     })
 }
