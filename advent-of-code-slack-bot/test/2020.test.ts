e60132b0c773499dda609037c2c5d6db976fa58a
fcamblor <fcamblor@gmail.com>
2020-12-16 12:49:49 +0100

    showing easter egg in daily statements

--- advent-of-code-slack-bot/test/2020.test.ts
+++ advent-of-code-slack-bot/test/2020.test.ts
@@ -2,6 +2,6 @@ import {testArticle} from "./tests-utils";
 
 
 test('ok', () => console.log("This is an empty test (to be able to execute the suite from IJ)"));
-for(var i=1; i<=5; i++) {
+for(var i=1; i<=16; i++) {
     testArticle("2020", `2020-${i<10?'0':''}${i}`);
 }
